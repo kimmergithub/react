9fa21e2d0f75f6b69d8a84a4da0b085050f53ad8
kimmergithub <kimmer@atomicprofits.com>
2017-08-02 11:35:36 -0700

    added webpack with modules, webpack called with a scripts.min.js page

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -1,9 +1,18 @@
 'use strict';
 
-require(./module1.js);
-require(./module2.js);
+require('./module1.js');
+require('./module2.js');
 
 // instructions
+//===============
+// in TERMINAL
+//===============
+// WEBPACK TUTORIAL:  https://www.youtube.com/watch?annotation_id=annotation_4139363737&feature=iv&src_vid=MhkGQAoc7bc&v=9kJVYpOqcVU
 // npm init
 // npm install -s webpack
-// 
+// npmm install -g webpack ==== installs is globally
+// touch webpack.config.js
+// got to this link and copy configurations into webpack.config.js
+// LINK:  https://gist.github.com/learncodeacademy/25092d8f1daf5e4a6fd3
+// make sure the webpack config file paths are correct, the run webpack
+// TERMINAL command === webpack
