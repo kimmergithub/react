2cd54c1533b727c9478dfbff34691505a719e963
kimmergithub <kimmer@atomicprofits.com>
2017-08-02 12:32:53 -0700

    adding terminal package to atom

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -4,9 +4,64 @@ require('./module1.js');
 require('./module2.js');
 
 // instructions
+//
+// FUNfunFUNfunctions === REACT
+//===============
+// NET Ninja
+//===============
+//
+// DOWNLOAD nodejs
+// download cmder (for windows) ... this is command line editor.
+//
+// npm init ===> get package.json file.
+//
+//LINKS WILL BE AT THE BOTTOM -- There is a quick way but we're going to do this the slow way.
+
+// LINKS:
+// Published on Oct 13, 2016
+// Hey ninjas, in this React tutorial I'll be showing you how to set up a basic work space for React using Webpack. I'll show you how to use the NPM to install React (and React DOM) as well as how to configure webpack to run with Babel.
+//
+// webpack config gist - https://gist.github.com/iamshaunjp/a1...
+//
+// ----- COURSE LINKS:
+// + Repo - https://github.com/iamshaunjp/react-p...
+// + Atom editor - https://atom.io/a
+// + Download GIT - https://git-scm.com/
+// + CMDER - http://cmder.net/
+//
+// JS Playlist - https://www.youtube.com/playlist?list...
+//
+// HTML Playlist - https://www.youtube.com/playlist?list...
+//
+// NODEJS Playlist - https://www.youtube.com/playlist?list...
+//
+//
+// ---------------------------------------------------------------------------------------------
+// You can find more front-end development tutorials on CSS, HTML, JavaScript, jQuery, WordPress & more on the channel homepage...
+//
+// SUBSCRIBE TO CHANNEL - https://www.youtube.com/channel/UCW5Y...
+//
+// ========== PSD to WordPress Playlist ==========
+//
+// https://www.youtube.com/playlist?list...
+//
+// ============ Node.js for Beginners Playlist =============
+//
+// https://www.youtube.com/playlist?list...
+//
+// ============== The Net Ninja =====================
+//
+// For more front-end development tutorials & to black-belt your coding skills, head over to - https://www.youtube.com/channel/UCW5Y... or http://thenetninja.co.uk
+//
+// ================== Social Links ==================
+//
+// Twitter - @TheNetNinja - https://twitter.com/thenetninjauk
+
+//
+
 //===============
 // in TERMINAL
-//===============
+//=============== // HAD ERRORS
 // WEBPACK TUTORIAL:  https://www.youtube.com/watch?annotation_id=annotation_4139363737&feature=iv&src_vid=MhkGQAoc7bc&v=9kJVYpOqcVU
 // npm init
 // npm install -s webpack
@@ -16,3 +71,7 @@ require('./module2.js');
 // LINK:  https://gist.github.com/learncodeacademy/25092d8f1daf5e4a6fd3
 // make sure the webpack config file paths are correct, the run webpack
 // TERMINAL command === webpack
+
+// NODE_ENV=production webpack ===> this will put the code into production mode? and minify the min?
+//
+//
